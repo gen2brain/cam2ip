f3b81f4aea35951dff515d883c4598da1c037000
bekapish <59603412+bekapish@users.noreply.github.com>
2020-10-01 13:32:09 +0530

    Update index.go

--- handlers/index.go
+++ handlers/index.go
@@ -1,21 +1,25 @@
 package handlers
 
-import (
+import 
+(
 	"net/http"
 )
 
 // Index handler.
-type Index struct {
+type Index struct
+{
 }
 
 // NewIndex returns new Index handler.
-func NewIndex() *Index {
+func NewIndex() *Index 
+{
 	return &Index{}
 }
 
 // ServeHTTP handles requests on incoming connections.
 func (i *Index) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" && r.Method != "HEAD" {
+	if r.Method != "GET" && r.Method != "HEAD" 
+	{
 		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
